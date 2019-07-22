f07398330b51e5a91ec8b5bdbac9ea96bae46ce1
Amy M Haddad <amy.m.haddad@gmail.com>
2019-07-22 10:27:08 -0400

    Created an "author" page that shows the list of authors and their book. Each name and book is linked to a page that provides detail on the specific book.

--- upcase_booklist_app/upcase_booklist_app.py
+++ upcase_booklist_app/upcase_booklist_app.py
@@ -1,8 +1,33 @@
-from flask import Flask 
+from flask import Flask, render_template, url_for
+from data import books
+
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    return "Booklist App"
+    """Render the homepage"""
+    return render_template("index.html")
+
+
+@app.route("/books")
+def book_list():
+    """Show the list of books"""
+    return render_template("books.html", books=books)
+
+
+@app.route("/books/<book_id>")
+def single_book(book_id):
+    """Show details for a specific book."""
+    return render_template("single_book.html", book=books[book_id])
+
+
+@app.route("/authors")
+def authors():
+    """Show the list of authors and the books they've written"""
+    return render_template("authors.html", books=books)
+
+
+if __name__ == "__main__":
+    app.run()
