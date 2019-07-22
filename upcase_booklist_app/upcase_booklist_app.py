9e698581434d805be51f71bcd0839eb9d8659677
Amy M Haddad <amy.m.haddad@gmail.com>
2019-07-22 10:27:08 -0400

    Created a homepage that links to a booklist page. Used Jinja and templates to get standard info to replicate on each page and specific info to render if provided. And I included a single book view.

--- upcase_booklist_app/upcase_booklist_app.py
+++ upcase_booklist_app/upcase_booklist_app.py
@@ -1,8 +1,61 @@
-from flask import Flask 
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
+books = {
+    "1": {
+        "title": "Python Crash Course",
+        "author": "Eric Matthes",
+        "summary": "A hands-on introduction to programming with Python.",
+        "date": "2016",
+        "image": "/static/img/python_crash_course.jpeg",
+    },
+    "2": {
+        "title": "Grokking Algorithms",
+        "author": "Aditya Bhargava",
+        "summary": "Learn how to apply common algorithms to practical programming problesm.",
+        "date": "2016",
+        "image": "/static/img/grokking_algorithms.jpeg",
+    },
+    "3": {
+        "title": "The Quick Python Book",
+        "author": "Naomi Ceder",
+        "summary": "A comprehensive guide to Python.",
+        "date": "2018",
+        "image": "/static/img/the_quick_python_book.jpeg",
+    },
+    "4": {
+        "title": "Irresistible APIs",
+        "author": "Kristen Hunter",
+        "summary": "Learn the process to create APIs",
+        "date": "2017",
+        "image": "/static/img/irresistible_APIs.jpeg",
+    },
+    "5": {
+        "title": "Introduction to Computer Science and Programming Using Python",
+        "author": "John Guttag",
+        "summary": "An introduction to programming and computational problem solving, using Python.",
+        "date": "2016",
+        "image": "/static/img/intro_to_computer_science.jpg",
+    },
+}
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    return "Booklist App"
+    return render_template("index.html")
+
+
+@app.route("/books")
+def book_list():
+    return render_template("books.html", books=books)
+
+
+@app.route("/books/<book_id>")
+def single_book(book_id):
+    return render_template("single_book.html", book=books[book_id])
+
+
+if __name__ == "__main__":
+    app.run()
