0d8d7cb44fc66868ad94023bfa41027fed6acc40
Amy M Haddad <amy.m.haddad@gmail.com>
2019-12-01 11:04:46 -0500

    Inserted category data into DB

--- scripts/import_categories.py
+++ scripts/import_categories.py
@@ -1,21 +1,17 @@
-
-
 from upcase_booklist_app.database import db_session
 from upcase_booklist_app.models.category import Category
 import random
 
-
-programming_categories = ["Beginner", "Intermediate", "Advanced"]
 session = db_session()
+programming_categories = ["Beginner", "Intermediate", "Advanced"]
+
 
 categories = []
 
 for _ in range(100):
-    category = Category(
-        category_name = random.choice(programming_categories)
-    )
+    category = Category(category_name=random.choice(programming_categories))
 
     categories.append(category)
 
-session.add_all(category)
+session.add_all(categories)
 session.commit()
